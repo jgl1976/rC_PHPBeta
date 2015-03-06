8305b14921f4178ac9f448d2b75f189f12e55903
John G. Levis Jr <jlevis@webitso.com>
2015-03-05 22:57:21 -0500

    config redirect

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define("CLIENT_ID", "3MVG9fMtCkV6eLhePwBjNw.lHHuAXPkLhDivF6di2chfNNQq2SNUn9qX0a.
 3
 define("CLIENT_SECRET", "1519327188401123285");
 4
-define("REDIRECT_URI", "https://localhost/resttest/oauth_callback.php");
+define("REDIRECT_URI", "https://fathomless-fjord-6605.herokuapp.com/resttest/oauth_callback.php");
 5
 define("LOGIN_URI", "https://login.salesforce.com");
 6
