464b6f561c0713646fa59ec4032cf6a228904f65
John G. Levis Jr <jlevis@webitso.com>
2015-03-05 22:54:08 -0500

    Config needs attention

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+2
+define("CLIENT_ID", "3MVG9fMtCkV6eLhePwBjNw.lHHuAXPkLhDivF6di2chfNNQq2SNUn9qX0a.phDXUj2TaAvOcJ4BolLqqiSluH");
+3
+define("CLIENT_SECRET", "1519327188401123285");
+4
+define("REDIRECT_URI", "https://localhost/resttest/oauth_callback.php");
+5
+define("LOGIN_URI", "https://login.salesforce.com");
+6
+?>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+02
+<html>
+03
+<head>
+04
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+05
+<title>REST/OAuth Example</title>
+06
+</head>
+07
+<body>
+08
+<script type="text/javascript" language="javascript">
+09
+    if (location.protocol != "https:") {
+10
+        document.write("OAuth will not work correctly from plain http. "+
+11
+                        "Please use an https URL.");
+12
+    } else {
+13
+        document.write("<a href=\"oauth.php\">Click here to retrieve accounts from Salesforce via REST/OAuth.</a>");
+14
+    }
+15
+</script>
+16
+</body>
+17
+</html>
