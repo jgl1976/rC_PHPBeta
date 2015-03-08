2582dc8d2182e092c5b65a5719d3edf885b9df40
Ryan Mazzucchi <rmazook4@yahoo.com>
2015-03-08 13:29:25 -0700

    test

--- layout.php
+++ layout.php
@@ -6,7 +6,7 @@ session_start();
 
 function show_accounts($instance_url, $access_token) {
 
-    $query = "SELECT Name, Id from Account LIMIT 100";
+    $query = "SELECT Name, Id from Opportunity LIMIT 100";
 
     $url = "$instance_url/services/data/v20.0/query?q=" . urlencode($query);
 
