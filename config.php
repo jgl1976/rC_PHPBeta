43949b1503f21979ffd091b0f81fffd9c726ec3d
Ryan Mazzucchi <rmazook4@yahoo.com>
2015-03-08 11:35:56 -0700

    try this

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define("CLIENT_ID", "3MVG9fMtCkV6eLhePwBjNw.lHHuAXPkLhDivF6di2chfNNQq2SNUn9qX0a.
 
 define("CLIENT_SECRET", "1519327188401123285");
 
-define("REDIRECT_URI", "https://fathomless-fjord-6605.herokuapp.com/resttest/oauth_callback.php");
+define("REDIRECT_URI", "https://fathomless-fjord-6605.herokuapp.com/oauth_callback.php");
 
 define("LOGIN_URI", "https://login.salesforce.com");
 
