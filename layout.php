51ef699de54f43f0908f4ee1d457bfb7799031d6
John G. Levis Jr <jlevis@webitso.com>
2015-03-08 15:16:47 -0400

    Helps to link

--- layout.php
+++ layout.php
@@ -274,7 +274,7 @@ function delete_account($id, $instance_url, $access_token) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
 <html>
-
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
     <head>
 
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -394,7 +394,7 @@ function delete_account($id, $instance_url, $access_token) {
     };
 </script>
 
-
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
 
     </body>
 
