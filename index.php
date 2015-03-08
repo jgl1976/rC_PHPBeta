b1e2de07e26449390a25de8e2343f528dc40537a
John G. Levis Jr <jlevis@webitso.com>
2015-03-08 17:04:46 -0400

    gateway style

--- index.php
+++ index.php
@@ -23,22 +23,21 @@
     <link href="css/bootstrap.css" rel="stylesheet">
     </head>
 <body>
+<div class="container">
+
+<h2 class="form-signin-heading">Please Sign into the roundCorner PHP Data Migration Automation App</h2>
 
 <script type="text/javascript" language="javascript">
-
     if (location.protocol != "https:") {
-
-        document.write("OAuth will not work correctly from plain http. "+
-
-                        "Please use an https URL.");
-
+        document.write("OAuth will not work correctly from plain http. "+ "Please use an https URL.");
     } else {
-
         document.write("<a href=\"oauth.php\" type=\"button\" class=\"btn btn-primary\">Sign In</a>");
-
     }
-
 </script>
+
+
+
+    </div> <!-- /container -->
 </body>
 
 </html>
