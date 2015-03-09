8a48058e9355fd502bbb3f072e24577bd12ac4a6
John G. Levis Jr <jlevis@webitso.com>
2015-03-08 23:08:09 -0400

    no more br

--- layout.php
+++ layout.php
@@ -42,7 +42,7 @@ function show_accounts($instance_url, $access_token) {
 
     foreach ((array) $response['records'] as $record) {
 
-        echo "<div class='container'><div class='table-responsive'><table class='table table-condensed table-hover'><td>".$record['Id'] ."</td><td>" . $record['Name'] . "</td><td>$" . $record['Amount'] . "</td></table></div></div><br/>";
+        echo "<div class='container'><div class='table-responsive'><table class='table table-condensed table-hover'><td>".$record['Id'] ."</td><td>" . $record['Name'] . "</td><td>$" . $record['Amount'] . "</td></table></div></div>";
 
     }
 
