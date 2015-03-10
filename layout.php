6848525fe3e4cafa1580a82ee83061c29db046f8
Ryan Mazzucchi <rmazook4@yahoo.com>
2015-03-10 11:20:08 -0700

    pagination test

--- layout.php
+++ layout.php
@@ -46,12 +46,77 @@ function show_accounts($instance_url, $access_token) {
     foreach ((array) $response['records'] as $record) {
 
         echo "<div class='container'><div class='table-responsive'><table class='table table-condensed table-hover'><tr><td width='33%'>".$record['Id']."</td><td width='33%'>".$record['Name']."</td><td width='33%'>$".$record['AnnualRevenue']."</td></tr></table></div></div>";
+		$theId = $record['Id'];
 
-    }if(isset($record['Id'])){
-		echo "ID is set";	
-	}else{
-		echo "no id is set";	
-	}
+    }//////////////////////////////pagination/////////////////////////////////////////////////////////////////
+
+if (isset($_POST['$record["Id"]']) && isset($_GET['pn'])) { // Get pn from URL vars if it is present
+    $pn = preg_replace('#[^0-9]#i', '', $_GET['pn']); // filter everything but numbers for security(new)
+    //$pn = ereg_replace("[^0-9]", "", $_GET['pn']); // filter everything but numbers for security(deprecated)
+} else { // If the pn URL variable is not present force it to be value of page number 1
+    $pn = 1;
+} 
+//This is where we set how many database items to show on each page 
+$itemsPerPage = 3; 
+// Get the value of the last page in the pagination result set
+$lastPage = ceil($num_rows / $itemsPerPage);
+// Be sure URL variable $pn(page number) is no lower than page 1 and no higher than $lastpage
+if ($pn < 1) { // If it is less than 1
+    $pn = 1; // force if to be 1
+} else if ($pn > $lastPage) { // if it is greater than $lastpage
+    $pn = $lastPage; // force it to be $lastpage's value
+} 
+// This creates the numbers to click in between the next and back buttons
+// This section is explained well in the video that accompanies this script
+$centerPages = "";
+$sub1 = $pn - 1;
+$sub2 = $pn - 2;
+$add1 = $pn + 1;
+$add2 = $pn + 2;
+if ($pn == 1) {
+    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?category='.$vidCategory.'&pn=' . $add1 . '">' . $add1 . '</a> &nbsp;';
+} else if ($pn == $lastPage) {
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?category='.$vidCategory.'&pn=' . $sub1 . '">' . $sub1 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
+} else if ($pn > 2 && $pn < ($lastPage - 1)) {
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?category='.$vidCategory.'&pn=' . $sub2 . '">' . $sub2 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?category='.$vidCategory.'&pn=' . $sub1 . '">' . $sub1 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?category='.$vidCategory.'&pn=' . $add1 . '">' . $add1 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?category='.$vidCategory.'&pn=' . $add2 . '">' . $add2 . '</a> &nbsp;';
+} else if ($pn > 1 && $pn < $lastPage) {
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?category='.$vidCategory.'&pn=' . $sub1 . '">' . $sub1 . '</a> &nbsp;';
+    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
+    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?category='.$vidCategory.'&pn=' . $add1 . '">' . $add1 . '</a> &nbsp;';
+}
+// This line sets the "LIMIT" range... the 2 values we place to choose a range of rows from database in our query
+$limit = 'LIMIT ' .($pn - 1) * $itemsPerPage .',' .$itemsPerPage; 
+// Now we are going to run the same query as above but this time add $limit onto the end of the SQL syntax
+// $sql2 is what we will use to fuel our while loop statement below
+$sql2 = "SELECT Id from Account WHERE Id='$theId' $limit"; 
+//////////////////////////////// END Adam's Pagination Logic ////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// Adam's Pagination Display Setup /////////////////////////////////////////////////////////////////////
+$paginationDisplay = ""; // Initialize the pagination output variable
+// This code runs only if the last page variable is ot equal to 1, if it is only 1 page we require no paginated links to display
+if ($lastPage != "1"){
+    // This shows the user what page they are on, and the total number of pages
+    $paginationDisplay .= '<div class="pagination">Page <strong>' . $pn . '</strong> of ' . $lastPage. '&nbsp;  &nbsp;  &nbsp; ';
+    // If we are not on page 1 we can place the Back button
+    if ($pn != 1) {
+        $previous = $pn - 1;
+        $paginationDisplay .=  '&nbsp;  <a href="' . $_SERVER['PHP_SELF'] . '?category='.$vidCategory.'&pn=' . $previous . '"> Back</a> ';
+    } 
+    // Lay in the clickable numbers display here between the Back and Next links
+    $paginationDisplay .= '<span class="paginationNumbers">' . $centerPages . '</span>';
+    // If we are not on the very last page we can place the Next button
+    if ($pn != $lastPage) {
+        $nextPage = $pn + 1;
+        $paginationDisplay .=  '&nbsp;  <a href="' . $_SERVER['PHP_SELF'] . '?category='.$vidCategory.'&pn=' . $nextPage . '"> Next</a></div>';
+    } 
+}
+
+//////////////////////////////pagination/////////////////////////////////////////////////////////////////	
 
     echo "<br/>";
 
@@ -359,6 +424,10 @@ function delete_account($id, $instance_url, $access_token) {
             ?>
 
         </tt>
+        <br/>
+
+        
+        <?php echo $paginationDisplay; ?>
  <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
