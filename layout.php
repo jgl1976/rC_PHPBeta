21f8bb40b4942ee6bef76596bd7266d2eea44289
Ryan Mazzucchi <rmazook4@yahoo.com>
2015-03-11 01:08:24 -0700

    Awe Yeah :)

--- layout.php
+++ layout.php
@@ -185,70 +185,9 @@ if ($lastPage != "1"){
 	$theDiv .= "</table></div></div>";
 	}
 
-    echo "<div class='container-fluid'><div class='bg-primary' align='center'><h2>$total_size record(s) returned</h2></div></div><br/><br/>
+    echo "<div class='container-fluid'><div class='bg-primary' align='center'><h2>Total Number Of Records: $total_size</h2></div></div><br/><br/>
     <div class='container'><div class='table-responsive' style='overflow: hidden;'><table class='table'><tr><td width='33%'><h3>ID</h3></td><td width='33%'><h3>Name</h3></td><td width='33%'><h3>AnnualRevenue</h3></td></tr></table>";	
 	echo $theDiv;
-
-/*This is where we set how many database items to show on each page 
-$itemsPerPage = 10; 
-
-// Get the value of the last page in the pagination result set
-$lastPage = ceil($total_size / $itemsPerPage);
-
-// Be sure URL variable $pn(page number) is no lower than page 1 and no higher than $lastpage
-if ($pn < 1) { // If it is less than 1
-    $pn = 1; // force if to be 1
-} else if ($pn > $lastPage) { // if it is greater than $lastpage
-    $pn = $lastPage; // force it to be $lastpage's value
-} 
-// This creates the numbers to click in between the next and back buttons
-// This section is explained well in the video that accompanies this script
-$centerPages = "";
-$sub1 = $pn - 1;
-$sub2 = $pn - 2;
-$add1 = $pn + 1;
-$add2 = $pn + 2;
-if ($pn == 1) {
-    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
-    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add1 . '">' . $add1 . '</a> &nbsp;';
-} else if ($pn == $lastPage) {
-    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub1 . '">' . $sub1 . '</a> &nbsp;';
-    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
-} else if ($pn > 2 && $pn < ($lastPage - 1)) {
-    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub2 . '">' . $sub2 . '</a> &nbsp;';
-    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub1 . '">' . $sub1 . '</a> &nbsp;';
-    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
-    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add1 . '">' . $add1 . '</a> &nbsp;';
-    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add2 . '">' . $add2 . '</a> &nbsp;';
-} else if ($pn > 1 && $pn < $lastPage) {
-    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub1 . '">' . $sub1 . '</a> &nbsp;';
-    $centerPages .= '&nbsp; <span class="pagNumActive">' . $pn . '</span> &nbsp;';
-    $centerPages .= '&nbsp; <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add1 . '">' . $add1 . '</a> &nbsp;';
-}
-// This line sets the "LIMIT" range... the 2 values we place to choose a range of rows from database in our query
-//$limit = 'LIMIT ' .($pn - 1) * $itemsPerPage .',' .$itemsPerPage;
-	
-//////Adam's Pagination Display Setup /////////////////////////////////////////////////////////////////////
-$paginationDisplay = ""; // Initialize the pagination output variable
-// This code runs only if the last page variable is ot equal to 1, if it is only 1 page we require no paginated links to display
-if ($lastPage != "1"){
-    // This shows the user what page they are on, and the total number of pages
-    $paginationDisplay .= '<div class="pagination">Page <strong>' . $pn . '</strong> of ' . $lastPage. '&nbsp;  &nbsp;  &nbsp; ';
-    // If we are not on page 1 we can place the Back button
-    if ($pn != 1) {
-        $previous = $pn - 1;
-        $paginationDisplay .=  '&nbsp;  <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $previous . '"> Back</a> ';
-    } 
-    // Lay in the clickable numbers display here between the Back and Next links
-    $paginationDisplay .= '<span class="paginationNumbers">' . $centerPages . '</span>';
-    // If we are not on the very last page we can place the Next button
-    if ($pn != $lastPage) {
-        $nextPage = $pn + 1;
-        $paginationDisplay .=  '&nbsp;  <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $nextPage . '"> Next</a></div>';
-    } 
-}
-
-//////////////////////////////pagination/////////////////////////////////////////////////////////////////*/
 }
 /*function create_account($name, $instance_url, $access_token) {
 
