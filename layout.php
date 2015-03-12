b3491649459cca85193e6dae6ba316320bcbed45
John G. Levis Jr <jlevis@webitso.com>
2015-03-12 01:04:36 -0400

    No

--- layout.php
+++ layout.php
@@ -180,12 +180,12 @@ if ($pn == 1) {
 } else if ($pn > 2 && $pn < ($lastPage - 1)) {
     $centerPages .= '&nbsp; <li><a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub2 . '">' . $sub2 . '</a></li> &nbsp;';
     $centerPages .= '&nbsp; <li><a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub1 . '">' . $sub1 . '</a></li> &nbsp;';
-    $centerPages .= '&nbsp; <li><span>' . $pn . '</span></li> &nbsp;';
+    $centerPages .= '&nbsp; <li class="active"><span>' . $pn . '</span></li> &nbsp;';
     $centerPages .= '&nbsp;<li> <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add1 . '">' . $add1 . '</a></li> &nbsp;';
     $centerPages .= '&nbsp;<li> <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add2 . '">' . $add2 . '</a></li> &nbsp;';
 } else if ($pn > 1 && $pn < $lastPage) {
     $centerPages .= '&nbsp; <li><a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $sub1 . '">' . $sub1 . '</a></li> &nbsp;';
-    $centerPages .= '&nbsp; <li><span>' . $pn . '</span></li> &nbsp;';
+    $centerPages .= '&nbsp; <li class="active"><span>' . $pn . '</span></li> &nbsp;';
     $centerPages .= '&nbsp; <li><a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $add1 . '">' . $add1 . '</a></li> &nbsp;';
 }
         
