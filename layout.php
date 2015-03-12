45a877189d8c191e531bbb7022905b0798a82854
John G. Levis Jr <jlevis@webitso.com>
2015-03-11 23:06:12 -0400

    got it

--- layout.php
+++ layout.php
@@ -94,7 +94,7 @@ $paginationDisplay = ""; // Initialize the pagination output variable
 // This code runs only if the last page variable is ot equal to 1, if it is only 1 page we require no paginated links to display
 if ($lastPage != "1"){
     // This shows the user what page they are on, and the total number of pages
-    $paginationDisplay .= '<ul class="pagination"><li><div class="pagination">Page <strong>' . $pn . '</strong> of ' . $lastPage. '&nbsp;  &nbsp;  &nbsp; <nav>
+    $paginationDisplay .= '<div class="pagination">Page <strong>' . $pn . '</strong> of ' . $lastPage. '&nbsp;  &nbsp;  &nbsp; <nav>
   <ul class="pagination">';
     // If we are not on page 1 we can place the Back button
     if ($pn != 1) {
