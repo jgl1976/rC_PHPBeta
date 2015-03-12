f2090b4c2fdfb175c09cd5995663371e984dc2d2
Ryan Mazzucchi <rmazook4@yahoo.com>
2015-03-12 15:07:00 -0700

    fixing arrow

--- layout.php
+++ layout.php
@@ -73,7 +73,7 @@ if ($lastPage != "1"){
   Page <strong>' . $pn . '</strong> of ' . $lastPage . '<br/>';
   //If we are on page one, generate disabled previous arrow
   if($pn == 1){
-		$paginationDisplay .= '<li class="disabled"><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>';
+		$paginationDisplay .= '<li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>';
   }
     // If we are not on page 1 we can place the Back button
     if ($pn > 1) {
