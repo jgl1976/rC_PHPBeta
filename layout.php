177d4468dc12c4d9bcdeb649b3ac0ba936fffe35
Ryan Mazzucchi <rmazook4@yahoo.com>
2015-03-13 11:57:13 -0700

    adasdad

--- layout.php
+++ layout.php
@@ -217,7 +217,7 @@ $searchBar = '<form method="get" action="'. $_SERVER['PHP_SELF'] . '" class="nav
         
         foreach ((array) $records as $record => $value) {
 			
-        echo $record;
+        echo $record['$value'];
 		
         $theDiv .= "<tr><td width='25%'>".$record['$choice1']."</td><td width='25%'>".$record['$choice2']."</td><td width='25%'>".$record['$choice3']."</td>
         <td width='25%'><button type='button' class='btn btn-warning'>Edit Record</button></td></tr>";
