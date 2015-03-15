dbd766a45757c54af6bb294b62b8181e8ef0bb7c
Ryan Mazzucchi <rmazook4@yahoo.com>
2015-03-14 18:40:34 -0700

    hard code id

--- layout.php
+++ layout.php
@@ -127,9 +127,9 @@ $searchBar = '<form method="get" action="'. $_SERVER['PHP_SELF'] . '" class="nav
         
         foreach ((array) $records as $record) {
 
-        echo "wtf".$record['$choice1'];
+       // echo "wtf".$record['$choice1'];
 		
-        $theDiv .= "<tr><td width='25%'>".$record["'$choice1'"]."</td><td width='25%'>".$record["'$choice2'"]."</td><td width='25%'>".$record["'$choice3'"]."</td>
+        $theDiv .= "<tr><td width='25%'>".$record['Id']."</td><td width='25%'>".$record["'$choice2'"]."</td><td width='25%'>".$record["'$choice3'"]."</td>
         <td width='25%'><button type='button' class='btn btn-warning'>Edit Record</button></td></tr>";
     }
         
