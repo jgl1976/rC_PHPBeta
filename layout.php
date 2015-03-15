f162f9025c8a5dffd8951a8b58124105e93f2662
Ryan Mazzucchi <rmazook4@yahoo.com>
2015-03-14 18:56:54 -0700

    no way

--- layout.php
+++ layout.php
@@ -126,9 +126,8 @@ $searchBar = '<form method="get" action="'. $_SERVER['PHP_SELF'] . '" class="nav
         $theDiv = "<br/><div class='container'><div class='table-responsive'><table class='table table-condensed table-hover'>";
         
         foreach ((array) $records as $record) {
-
-        echo $choice1;
-		echo $choice2;
+			
+			echo "what?";
 		
         $theDiv .= "<tr><td width='25%'>".$record[$choice1]."</td><td width='25%'>".$record[$choice2]."</td><td width='25%'>".$record[$choice3]."</td>
         <td width='25%'><button type='button' class='btn btn-warning'>Edit Record</button></td></tr>";
