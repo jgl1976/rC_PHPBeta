16baf18dadc9060db42e5de6192504472b334655
John G. Levis Jr <jlevis@webitso.com>
2015-03-18 18:43:53 -0400

    shit and giggles

--- db/connect_to_mysql.php
+++ db/connect_to_mysql.php
@@ -10,5 +10,5 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+echo "Connected successfully to rC custom dataMigration DataBase";
 ?>
