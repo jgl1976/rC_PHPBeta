a57efd20d2dec6c6e5aceec2bbca7205532dbde6
John G. Levis Jr <jlevis@webitso.com>
2015-03-18 14:00:00 -0400

    syntax

--- editRecord.php
+++ editRecord.php
@@ -8,8 +8,8 @@ if(isset($_POST['rId'])){
 	header("location: layout.php");
 }
 
-echo "<br><br><div class='container-fluid'><div class='bg-primary' align='center'><h2>$choice</h2></div></div><br/><div class='container'><div class='row'>
-<div class='col-md-4'>
+echo "<br><br><div class='container-fluid'><div class='bg-primary' align='center'><h2>".$record[$choice]."</h2></div></div><br/>
+<div class='container'><div class='row'><div class='col-md-4'>
 <button onclick='goBack()' type='button' class='btn btn-primary btn-lg'>Go Back</button>
 <script>
 function goBack() {
