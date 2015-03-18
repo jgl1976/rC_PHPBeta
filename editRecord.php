73e9fa95b5e292d6296563e42e1a74b8aea6ac6d
John G. Levis Jr <jlevis@webitso.com>
2015-03-18 13:20:28 -0400

    Beta Testing in wrong App

--- editRecord.php
+++ editRecord.php
@@ -9,7 +9,7 @@ if(isset($_POST['rId'])){
 }
 
 echo "<br><br><div class='container'><div class='table-responsive' style='overflow: hidden;'><table class='table table-condensed table-hover'><tr><td>Data Here</td></tr></table></div><div class='row'><div class='col-md-4'></div><div class='col-md-4'><form>
-<div class='form-group'><label for='field1'>$id</label><input type='text' class='form-control' id='field1' placeholder='Edit Field'></div>
+<div class='form-group'><label for='field1'>$id</label><input type='text' class='form-control' id='field1' placeholder='$id'></div>
 <div class='form-group'><label for='field2'>$choice2</label><input type='text' class='form-control' id='$choice2' placeholder='Edit Field'></div>
 <div class='form-group'><label for='field3'>$choice3</label><input type='text' class='form-control' id='$choice3' placeholder='Edit Field'></div>
 </form></div><div class='col-md-4'></div></div></div>";
