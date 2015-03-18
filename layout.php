92c0f580cc3aa75a6bd9cfd3e24b9b039ea6490b
John G. Levis Jr <jlevis@webitso.com>
2015-03-18 07:43:02 -0400

    this wont work

--- layout.php
+++ layout.php
@@ -10,11 +10,11 @@ $choice = "";
 
 if(!isset($_GET['objectChosen'])){
 	$choice = "Opportunity";
-    $choice1 = "Id";
-    $choice2 = "rC_Giving__Current_Giving_Amount__c";
-    $choice3 = "rC_Giving__Expected_Giving_Amount__c";  
 }else{
 	$choice = $_GET['objectChosen'];	
+        $choice1 = "Id";
+    $choice2 = "rC_Giving__Current_Giving_Amount__c";
+    $choice3 = "rC_Giving__Expected_Giving_Amount__c";  
 }
 
 /*if($choice == "Account"){
