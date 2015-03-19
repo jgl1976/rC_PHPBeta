44d6b0cb5a29c03e0dad9a6063f5ad4459a3dd64
Ryan Mazzucchi <rmazook4@yahoo.com>
2015-03-19 14:02:29 -0700

    adding spaces between error messages

--- db/create_table.php
+++ db/create_table.php
@@ -17,10 +17,10 @@ if ($conn->connect_error) {
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully to rC custom dataMigration DataBase";
+echo "Connected successfully to rC custom dataMigration DataBase!  ";
 
 // sql to create table
-$sql = "CREATE TABLE rC_Opportunity (
+$sql = "CREATE TABLE IF NOT EXISTS rC_Opportunity (
 id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 sF_id VARCHAR(255) NOT NULL,
 sF_rC_Giving__Giving_Type__c VARCHAR(255) NOT NULL,
