42e06855ca2b19d31c38ac614f0ec86db97098d1
Ryan Mazzucchi <rmazook4@yahoo.com>
2015-03-19 18:08:13 -0700

    insertRecord.php cleaned up editRecord added values edited connection file

--- db/connect_to_mysql.php
+++ db/connect_to_mysql.php
@@ -2,13 +2,13 @@
 $servername = "ih7lzhkhj9ckxv8y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username = "fdo9nglcafpld48u";
 $password = "6c3pjezu2eteduv9";
+$dbname = "DCoE";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully to rC custom dataMigration DataBase";
 ?>
---
+++ insertRecord.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once ("db/connect_to_mysql.php");
+
+$msg = "";
+
+if(isset($_POST['id'])){
+	$id = $_POST['id'];
+	$choice2 = $_POST['choice2'];
+	$choice3 = $_POST['choice3'];
+    $choice4 = $_POST['choice4'];
+    $choice5 = $_POST['choice5'];
+    $choice6 = $_POST['choice6'];
+	
+	echo $id . ', '. $choice2 . ', ' . $choice3 . ', ' . $choice4 . ', ' . $choice5 . ', ' . $choice6;
+
+//$sql = "INSERT INTO $dbname (1,2,3) VALUES (1,2,3,) WHERE id='$id' LIMIT 1";
+
+
+}
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Insert Record</title>
+</head>
+
+<body>
+<?php echo $msg; ?>
+</body>
+</html>
