7a219e53a2e0d239995c5320dfe7d47db51ea12b
Ryan Mazzucchi <rmazook4@yahoo.com>
2015-03-19 18:45:53 -0700

    <<retard

--- insertRecord.php
+++ insertRecord.php
@@ -18,8 +18,8 @@ if(isset($_POST['id'])){
 	$fieldName5 = 'sF_' . $_POST['fieldName5'];
 	$fieldName6 = 'sF_' . $_POST['fieldName6'];
 	
-	echo $id . ', '. $choice2 . ', ' . $choice3 . ', ' . $choice4 . ', ' . $choice5 . ', ' . $choice6 . '<br/>';
-	//echo $fieldName1 . ', '. $fieldName2 . ', ' . $fieldName3 . ', ' . $fieldName4 . ', ' . $fieldName5 . ', ' . $fieldName6;
+	//echo $id . ', '. $choice2 . ', ' . $choice3 . ', ' . $choice4 . ', ' . $choice5 . ', ' . $choice6 . '<br/>';
+	echo $fieldName1 . ', '. $fieldName2 . ', ' . $fieldName3 . ', ' . $fieldName4 . ', ' . $fieldName5 . ', ' . $fieldName6;
 	
 
 /*$sql = "INSERT INTO $dbname ($fieldName1,$fieldName2,$fieldName3,$fieldName4,$fieldName5,$fieldName6) VALUES ($id,choice2,$choice3,$choice4,$choice5,$choice6) WHERE id='$id' LIMIT 1";
