6828c500fa22550920e65289a36e35070c5f1724
John G. Levis Jr <jlevis@webitso.com>
2015-03-20 22:06:59 -0400

    test_db

---
+++ list_mysqli.php
@@ -0,0 +1,28 @@
+<?php
+include_once ("db/connect_to_mysql.php");
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+/* return name of current default database */
+if ($result = $mysqli->query("SELECT DATABASE()")) {
+    $row = $result->fetch_row();
+    printf("Default database is %s.\n", $row[0]);
+    $result->close();
+}
+
+/* change db to world db 
+$mysqli->select_db("world");
+*/
+/* return name of current default database */
+if ($result = $mysqli->query("SELECT DATABASE()")) {
+    $row = $result->fetch_row();
+    printf("Default database is %s.\n", $row[0]);
+    $result->close();
+}
+
+$mysqli->close();
+?>
